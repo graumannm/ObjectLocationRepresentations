8c663b843c512548de1832ddb17c3beefa60975a
graumannm <monika.graumann@outlook.de>
2021-02-01 12:17:55 +0100

    cleaned up comments

--- Code/plot_DNN.m
+++ Code/plot_DNN.m
@@ -1,15 +1,20 @@
-function plot_DNN(myData,layers)
+function plot_DNN
+% function for plotting the result of classification of object location
+% across categories in layers of CORnet-S.
+% run this from main folder.
 
 layers   = {'V1' 'V2' 'V4' 'IT'};
+DNN      = 'CORnet-S';
 myData   = [];
 
 for ilayer=1:4
     
-    load(['./Results/CORnet-S/' layers{ilayer} '.mat'])
+    % load data
+    load(['./Results/' DNN '/' layers{ilayer} '.mat'])
     myData = [myData result' ]; clear result
 end
 
-%% Figure Setup
+%% figure setup
 hf = figure('position',[1,1,1000, 750], 'unit','pixel');
 set(gcf,'PaperUnits','centimeters','PaperSize',[30,30],'PaperPosition',[0,0,30,30]);
 colordef white
@@ -19,8 +24,7 @@ set(0,'DefaultAxesFontSize',25)
 set(0,'DefaultTextFontSize',25)
 set(gcf,'Color','w')
 
-%% Barplot
-% subplot(2,2,1)
+%% barplot
 hold on
 
 %colors
@@ -28,7 +32,7 @@ c1 = [0.6,0.6,0.6];
 c2 = [0,1,0];
 c3 = [0,0,1];
 
-%fake bars (for legend)
+% fake bars (for legend)
 h=bar(0,0);
 set(h,'facecolor',c1);
 set(h,'linewidth',3);
@@ -39,30 +43,26 @@ h=bar(0,0);
 set(h,'facecolor',c3);
 set(h,'linewidth',3);
 
-%means
-% m = mean(myData); %1 x myData(:)15
 
-%x axis position: wide distance=1.1; narrow=0.9
-% xb = [1.05,1.95,  3.05,3.95  ,5.05,5.95  ,7.05,7.95];
 xb = nan(length(myData)); % vector containing bar positions
 xb(1,1)=1.05;
 for j = 2:15
     
     if ismember(j,[4,7,10,13]) % big step before new ROI
-        xb(j)=xb(j-1)+1.6
+        xb(j)=xb(j-1)+1.6;
     else
-        xb(j)=xb(j-1)+0.85 % small step between bgs
+        xb(j)=xb(j-1)+0.85; % small step between background conditions
     end
     
 end
 
-% plot stuff
-for i=1:size(myData,2) % same as length of m
+% plot it
+for i=1:size(myData,2)
     
     
     % plot the bar
-    h = bar(xb(i),myData(i)); % xb says where m(i) what
-    if ismember(i,[1,4,7,10,13]) % nobg
+    h = bar(xb(i),myData(i)); % xb says where m(i) belongs
+    if ismember(i,[1,4,7,10,13]) % no clutter
         set(h,'facecolor',c1);
     elseif ismember(i,[2,5,8,11,14]) % low clutter
         set(h,'facecolor',c2);
@@ -73,16 +73,16 @@ for i=1:size(myData,2) % same as length of m
     
 end
 
-%Legend
+% legend
 L=legend({'No Background','Low Clutter','High Clutter'});
 set(L,'box','off');
 
-%other plot properties
-ylabel('Decoding Accuracy (%)');
-xlabel('')%('ROI');
+% other plot properties
+ylabel('Classification Accuracy (%)');
+xlabel([DNN ' layer'])
 set(gca,'linewidth',3);
-set(gca,'xtick',[xb([2,5,8,11],1)]);%[1.5,3.5]); % where to put labels
-set(gca,'xticklabel',layers);%'V1' 'V2' 'V4' 'IT'});
+set(gca,'xtick',[xb([2,5,8,11],1)]); % where to put labels
+set(gca,'xticklabel',layers);
 axis tight
 yl=get(gca,'ylim');
 ylim([0,yl(2)*1.25]);
