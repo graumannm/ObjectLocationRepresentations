7aa8761b82d0715fda9d68949ace83ca6ce2a150
graumannm <monika.graumann@outlook.de>
2021-02-01 12:03:59 +0100

    plot searchlight function

---
+++ Code/plot_searchlight.m
@@ -0,0 +1,25 @@
+function plot_searchlight(sbj,BG)
+% plot axial slices of searchlight classification of object location across
+% categories.
+
+% Input:
+%       sbj: subject's number, integer
+%       BG: background,integer. 1=no, 2=low, 3= high clutter
+
+task                  = 'Location';
+background_conditions = {'No','Low','High'};
+
+% load searchlight .mat file
+load(sprintf(['./Results/fMRI/Searchlight/s%.2d_' task '_SL_' background_conditions{BG} '_Clutter.mat'],sbj));
+
+count=0;
+figure;
+
+for slice=12:2:60
+    
+    count=count+1;
+    subplot(5,5,count)
+    imagesc(squeeze(SL(:,:,slice))); axis square; colorbar
+    
+    sgtitle([background_conditions{BG} ' Clutter'])
+end
