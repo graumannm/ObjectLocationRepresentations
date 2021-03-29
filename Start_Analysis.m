ea4295f363e8b5e298c58712982eec905fa2b25d
graumannm <monika.graumann@outlook.de>
2021-03-29 15:36:26 +0200

    updated for all subjects in ROI, no predefined results or figures folders

--- Start_Analysis.m
+++ Start_Analysis.m
@@ -4,30 +4,36 @@ clear
 clc
 addpath(genpath('Code'));
 addpath(genpath('Data'));
-addpath(genpath('Results'));
+spm_path = '/scratch/monikag/Toolboxes/spm8'; % insert you path to the SPM toolbox here. Only needed for searchlight
 
 %% set parameters for EEG time-resolved and time-generalization analysis
-
 steps        = 10; % downsample EEG data to 10 ms for speed. Set steps=1 for full 1 ms temporal resolution.
 permutations = 5; % 5 permutations for speed. This gives a close approximation to the result with full 100 permutations (used in paper).
 
+%% set parameters for ROI analysis
+subs  = 25;
+nROIs = 9;
+BG    = 3;
+ROIs  = nan(subs,nROIs*BG);
+
 %% Durations
 
-% Durations time-resolved analyses:
+% Durations time-resolved analyses (example subject):
 % 5 permutations downsampled to 10 ms resolution takes ~37 seconds (fast parameters).
 % 100 permutations with 1 ms resulution takes ~2 hours (original parameters).
 
-% Durations time-generalization:
+% Durations time-generalization (example subject):
 % 5 permutations downsampled to 10 ms resolution takes ~4 minutes (fast parameters).
 % 100 permutations downsampled to 10 ms takes ~85 minutes (original parameters).
 
-% Duration ROI analysis:
-% ~ 3 seconds
+% Duration ROI analysis (all subjects):
+% ~ 2 minutes
 
 %% prompt user input
 
-disp('This is a quick demo of the analyses for a representative single subject.')
+disp('This is a quick demo of the analyses for a representative single subject for speed (EEG) or the entire dataset (ROI).')
 disp('Please bear in mind that single subject data can be noisier than the group averaged result.')
+disp('The result can vary slightly from time to time due to random assignment of trials to bins and training and testing set.')
 
 task = input(' \n Would you like to see results for location (1) or category classification (2)?');
 
@@ -49,8 +55,13 @@ end
 if task ==1 % location analyses
     
     if analysis==1 % ROI
-        Location_ROI(sbj);
-        plot_ROI(sbj,task);
+        
+        for isub = 1:subs
+            result       =  Location_ROI(isub);
+            result       = result';
+            ROIs(isub,:) = result(:); clear result
+        end
+        plot_ROI(ROIs,task);
         
     elseif analysis==2 % timecourse
         Location_Time_Resolved(steps,permutations,sbj);
@@ -64,8 +75,13 @@ if task ==1 % location analyses
 else % category analyses
     
     if analysis==1 % ROI
-        Category_ROI(sbj);
-        plot_ROI(sbj,task);
+        
+        for isub = 1:subs
+            result       =  Category_ROI(isub);
+            result       = result';
+            ROIs(isub,:) = result(:); clear result
+        end
+        plot_ROI(ROIs,task);
         
     elseif analysis==2 % timecourse
         Category_Time_Resolved(steps,permutations,sbj);
@@ -81,28 +97,22 @@ end
 %% Optional: Searchlight analysis (~16 hours, 2 GB memory)
 % You also have the option to run the searchlight classification analysis
 % of object location across categories. This analysis takes ~16 hours per
-% background condition. Because of the long duration, the results
-% for this analysis are already saved in ./Results/fMRI/Searchlight/
+% background condition.
 
-% to run this analysis uncomment the following lines:
+% to run this analysis uncomment the following 3 lines:
 
-% BG = 1; % 1= no, 2= low, 3= high clutter
-% Location_Searchlight(1,BG); 
+% addpath(genpath(spm_path));
+% iBG     = 1; % 1= no, 2= low, 3= high clutter
+% subject = 1;
+% Location_Searchlight(subject,iBG);
 
 % to plot the (existing) result uncomment and run this line:
 
-% plot_searchlight(1,BG);
-
-% These results figures are already saved in ./Figures as
-% Location_Searchlight_NoClutter.fig
-% Location_Searchlight_LowClutter.fig
-% Location_Searchlight_HighClutter.fig
+% plot_searchlight(1,iBG);
 
 %% Optional: DNN analysis (~40 minutes with 5 permutations)
 % You also have the option to run the DNN classification analysis
-% of object location across categories. This analysis takes ~10 minutes per layer with 5 permutations. 
-% Because of the long duration, the results
-% for this analysis are already saved in ./Results/CORnet-S/
+% of object location across categories. This analysis takes ~10 minutes per layer with 5 permutations.
 
 % to run this analysis uncomment the following lines:
 
@@ -111,6 +121,3 @@ end
 %     Location_DNN(permutations,ilayer);
 % end
 % plot_DNN;
-
-% The results figure is saved in ./Figures as
-% Location_CORnet-S.fig
