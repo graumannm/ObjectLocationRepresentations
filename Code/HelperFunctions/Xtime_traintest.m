ab02506b042e006b6338447806cfb846e044c025
graumannm <monika.graumann@outlook.de>
2021-04-26 16:16:41 +0200

    preallocate time matrix

--- Code/HelperFunctions/Xtime_traintest.m
+++ Code/HelperFunctions/Xtime_traintest.m
@@ -3,7 +3,7 @@ function [time_matrix] = Xtime_traintest(trainA,trainB,testA,testB,timewindow,la
 % train on timepoint t and test on all other timepoints
 
 % train
- time_matrix  = length(timewindow);
+ time_matrix  = nan(1,length(timewindow));
  train_data   = [squeeze(trainA); squeeze(trainB)];
  model        = libsvmtrain(labels_train, train_data,'-s 0 -t 0 -q');
  
