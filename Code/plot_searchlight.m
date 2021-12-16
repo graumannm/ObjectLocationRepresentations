860c76d7879ccb1f60bf763f418d0136e6bf20e7
graumannm <monika.graumann@outlook.de>
2021-12-16 11:21:20 +0100

    decreased font size

--- Code/plot_searchlight.m
+++ Code/plot_searchlight.m
@@ -10,10 +10,13 @@ task                  = 'Location';
 background_conditions = {'No','Low','High'};
 
 % load searchlight .mat file
-load(sprintf(['./Results/fMRI/Searchlight/s%.2d_' task '_SL_' background_conditions{BG} '_Clutter.mat'],sbj));
+load(sprintf(['./Results/fMRI/Searchlight/s%.2d/s%.2d_' task '_SL_' background_conditions{BG} '_Clutter.mat'],sbj,sbj));
 
 count=0;
 figure;
+
+set(0,'DefaultAxesFontSize',5)
+set(0,'DefaultTextFontSize',5)
 
 for slice=12:2:60
     
